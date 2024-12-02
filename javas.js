5d8d4e09f7a39bd15fd01846ee12c3211f0899bf
Begeken <berkayekmn@gmail.com>
2024-12-02 15:56:27 +0300

    Add files via upload

---
+++ javas.js
@@ -0,0 +1,8 @@
+let score = 0;
+const scoreDisplay = document.getElementById('score');
+const clickButton = document.getElementById('clickButton');
+
+clickButton.addEventListener('click', () => {
+    score += 1;
+    scoreDisplay.textContent = `Score: ${score}`;
+});
